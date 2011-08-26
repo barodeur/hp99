4936267cbd39633a86ecf55a0f5062ea2d6fa79e
Paul Chobert <paul@chobert.fr>
2011-08-26 11:49:11 +0200

    fix production config

--- config/unicorn_production.rb
+++ config/unicorn_production.rb
@@ -27,7 +27,7 @@ working_directory APP_PATH + "current/" # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-listen "/tmp/hp99.staging.socket", :backlog => 64
+listen "/tmp/hp99.socket", :backlog => 64
 #listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
