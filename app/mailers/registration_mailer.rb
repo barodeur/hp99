63e6e2025d2338323687ade295bd99b114314e2f
Paul Chobert <paul@chobert.fr>
2011-08-28 11:14:33 +0200

    change registration mail sender

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,5 +1,5 @@
 class RegistrationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "paul@hp99.fr"
 
   def confirmation(registration)
     @registration = registration
