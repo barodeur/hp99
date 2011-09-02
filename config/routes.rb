ed9ea1a88dc124aae303157877e3c6f452680913
Paul Chobert <paul@chobert.fr>
2011-09-02 04:05:12 +0200

    setup backoffice

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Hp99::Application.routes.draw do
   root :to => 'registrations#new'
+
+  devise_for :admins
+
+  namespace :admin do
+  end
+
   resources :registrations, :only => [:show, :new, :create], :path => 'r' do
     get :confirmation, :on => :member
     get :unconfirm, :on => :member
